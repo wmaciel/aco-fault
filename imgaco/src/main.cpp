0df09d2d60a6891cb33a4395647398b8c3330857
Walther Maciel <wmaciel@tecgraf.puc-rio.br>
2013-05-06 19:30:20 +0000

    faz a chamada dos métodos da main
    git-svn-id: https://subversion.tecgraf.puc-rio.br/svn/svi/trunk/prototipos/antColonyOptimization@38277 67483ca6-7138-0410-9db0-fa9ffb5cce8b

--- imgaco/src/main.cpp
+++ imgaco/src/main.cpp
@@ -1,4 +1,34 @@
-int main()
+
+#include <iostream>
+#include <stdlib.h>
+
+#include "image.h"
+#include "Colony.h"
+
+int main( int argc, char** argv )
 {
+    srand(time(0));
+
+    if (argc != 4)
+    {
+        std::cerr << "Usage: imgaco [input.bmp] [output.bmp] [number of iterations]\n";
+        return 0;
+    }
+
+    Image* input = imgReadBMP( argv[1] );
+    if ( !input )
+    {
+        std::cerr << "Image " << argv[1] << "could not be loaded\n";
+        return 0;
+    }
+
+    Colony colony( input, 1000 );
+    int nIterations = strtol( argv[3], NULL, 10 );
+    colony.run( nIterations );
+
+    Image* pheromone = colony.getPheromoneImage();
+
+    imgWriteBMP( argv[2], pheromone );
+
     return 0;
 }
