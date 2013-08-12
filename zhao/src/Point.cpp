6aff3e7b37c13d88d9c352a2d43f5c054bacdbfa
Walther Maciel <wmaciel@tecgraf.puc-rio.br>
2013-08-12 21:41:46 +0000

    implementa operador de soma nos pontos
    git-svn-id: https://subversion.tecgraf.puc-rio.br/svn/svi/trunk/prototipos/antColonyOptimization@41681 67483ca6-7138-0410-9db0-fa9ffb5cce8b

--- zhao/src/Point.cpp
+++ zhao/src/Point.cpp
@@ -33,3 +33,9 @@ bool operator==(Point p1, Point p2)
 {
     return (p1.x == p2.x) && (p1.y == p2.y);
 }
+
+Point& operator+(Point p1, Point p2)
+{
+    Point* pSum = new Point( p1.x + p2.x, p1.y + p2.y );
+    return *pSum;
+}
