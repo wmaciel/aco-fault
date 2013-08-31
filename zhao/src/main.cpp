c3a0c577cce6d7f92b20f190f7a5aa94edcf2fe3
Walther Maciel <wmaciel@tecgraf.puc-rio.br>
2013-08-31 20:40:54 +0000

    Passa o low pass filter na imagem da impressão digita antes de rodar o campo direcional
    git-svn-id: https://subversion.tecgraf.puc-rio.br/svn/svi/trunk/prototipos/antColonyOptimization@42273 67483ca6-7138-0410-9db0-fa9ffb5cce8b

--- zhao/src/main.cpp
+++ zhao/src/main.cpp
@@ -35,66 +35,69 @@
 //    field->debugPrint();
 //}
 //
-//void testImage( char* fileIn, char* fileDir, char* fileCoh, char* fileMask )
-//{
-//    Image* img = imgReadBMP( fileIn );
-//    Image* grey = imgGrey( img );
-//
-//    float* data = imgGetData( grey );
-//    int width = imgGetWidth(grey);
-//    int height = imgGetHeight(grey);
-//    DirectionalField* field = new DirectionalField( data, width, height, 9 );
-//
-//    Image* dir = imgCreate( width, height, 3 );
-//    Image* coh = imgCreate( width, height, 3 );
-//    Image* mask = imgCreate( width, height, 3 );
-//    for (int x = 0; x < width; ++x)
-//    {
-//        for (int y = 0; y < height; ++y)
-//        {
-//            float dirX = field->getXDirection( x, y );
-//            float dirY = field->getYDirection( x, y );
-//            float lumCoh;
-//            float lumX;
-//            float lumY;
-//
-//            imgSetPixel3f( dir, x, y, 1,1,1 );
-//
-//            if (dirX == 0.0f && dirY == 0.0f)
-//            {
-//                imgSetPixel3f( dir, x, y, 1.0, 1.0, 0.0 );
-//            }
-//            else
-//            {
-//                if (dirX < 0)
-//                {
-//                    dirY *= -1;
-//                    dirX *= -1;
-//                }
-//
-////                lumX = fabs( dirX );
-////                lumY = fabs( dirY );
-//                float lum = ( dirY + 1.0 ) * 0.5f;
-//                imgSetPixel3f( dir, x, y, lum, lum, lum);
-//            }
-//
-//            lumCoh = field->getCoherence( x, y );
-//            imgSetPixel3f( coh, x, y, lumCoh, lumCoh, lumCoh );
-//            if (lumCoh >= 0.35f)
-//            {
-//                imgSetPixel3f( mask, x, y, 1.0f, 1.0f, 1.0f );
-//            }
-//            else
-//            {
-//                imgSetPixel3f( mask, x, y, 0.0f, 0.0f, 0.0f );
-//            }
-//        }
-//    }
-//
-//    imgWriteBMP( fileDir, dir );
-//    imgWriteBMP( fileCoh, coh );
-//    imgWriteBMP( fileMask, mask );
-//}
+void testImage( char* fileIn, char* fileDir, char* fileCoh, char* fileMask )
+{
+    Image* img = imgReadBMP( fileIn );
+    Image* grey = imgGrey( img );
+    Image* greyGauss = imgCopy( grey );
+    imgGauss( greyGauss, grey );
+
+    float* data = imgGetData( greyGauss );
+    int width = imgGetWidth(greyGauss);
+    int height = imgGetHeight(greyGauss);
+    DirectionalField* field = new DirectionalField( data, width, height, 8 );
+
+    Image* dir = imgCreate( width, height, 3 );
+    Image* coh = imgCreate( width, height, 3 );
+    Image* mask = imgCreate( width, height, 3 );
+    for (int x = 0; x < width; ++x)
+    {
+        for (int y = 0; y < height; ++y)
+        {
+            float dirX = field->getXDirection( x, y );
+            float dirY = field->getYDirection( x, y );
+            float lumCoh;
+            float lumX;
+            float lumY;
+
+            imgSetPixel3f( dir, x, y, 1,1,1 );
+
+            if (dirX == 0.0f && dirY == 0.0f)
+            {
+                imgSetPixel3f( dir, x, y, 1.0, 1.0, 0.0 );
+            }
+            else
+            {
+                if (dirX < 0)
+                {
+                    dirY *= -1;
+                    dirX *= -1;
+                }
+
+//                lumX = fabs( dirX );
+//                lumY = fabs( dirY );
+                float lum = ( dirY + 1.0 ) * 0.5f;
+                imgSetPixel3f( dir, x, y, lum, lum, lum);
+            }
+
+            lumCoh = field->getCoherence( x, y );
+            imgSetPixel3f( coh, x, y, lumCoh, lumCoh, lumCoh );
+            if (lumCoh >= 0.35f)
+            {
+                imgSetPixel3f( mask, x, y, 1.0f, 1.0f, 1.0f );
+            }
+            else
+            {
+                imgSetPixel3f( mask, x, y, 0.0f, 0.0f, 0.0f );
+            }
+        }
+    }
+
+    imgWriteBMP( "gaussiana.bmp", greyGauss );
+    imgWriteBMP( fileDir, dir );
+    imgWriteBMP( fileCoh, coh );
+    imgWriteBMP( fileMask, mask );
+}
 
 //void whoLetTheAntsOut( int argc, char** argv )
 //{
@@ -111,18 +114,22 @@ void testImageMorphology()
 {
     Image* aux = imgReadBMP( "morphology/print_mask.bmp" );
     Image* in = imgGrey( aux );
-    Image* kernel = imgReadBMP( "morphology/kernel.bmp" );
+    Image* kernel = imgReadBMP( "morphology/kernelBig.bmp" );
 
-    Image* dilated = imgDilate( in, kernel );
-    Image* eroded  = imgErode( in, kernel );
+    Image* eroded = imgErode( in, kernel );
+    Image* opened = imgDilate( eroded, kernel );
+    Image* openedDilated = imgDilate( opened, kernel );
+    Image* closed = imgErode( openedDilated, kernel );
 
-    imgWriteBMP( "morphology/dilated.bmp", dilated );
     imgWriteBMP( "morphology/eroded.bmp", eroded );
+    imgWriteBMP( "morphology/opened.bmp", opened );
+    imgWriteBMP( "morphology/openedDilated.bmp", openedDilated );
+    imgWriteBMP( "morphology/closed.bmp", closed );
 }
 
 int main( int argc, char** argv )
 {
-    //testImage( "sint.bmp", "sint_dir.bmp", "sint_coh.bmp", "sint_mask.bmp" );
+    //testImage( "print.bmp", "print_dir.bmp", "print_coh.bmp", "print_mask.bmp" );
     //basicTest();
     //whoLetTheAntsOut( argc, argv );
     testImageMorphology();
