398827dd5cc9a9d411bc7a0a3bfa417a2a3b0eec
Walther Maciel <wmaciel@tecgraf.puc-rio.br>
2013-11-03 19:37:43 +0000

    documentação
    git-svn-id: https://subversion.tecgraf.puc-rio.br/svn/svi/trunk/prototipos/antColonyOptimization@44085 67483ca6-7138-0410-9db0-fa9ffb5cce8b

--- zhao/src/Ant.h
+++ zhao/src/Ant.h
@@ -17,14 +17,62 @@ class Environment;
 class Ant
 {
 public:
+    /**
+     * Constructor
+     * @param point Pixel where the ant will be born in
+     * @param environment Environment the ant will live in
+     */
     Ant( Point point, Environment* environment );
+    
+    /**
+     * Destructor
+     */
     virtual ~Ant();
+    
+    /**
+     * Defines how much an ant can move. The stepLength corresponds to half the 
+     * width of box around the the ant in which it can move.
+     * @param stepLength Half width of the box in wich the ant can move
+     */
     void setStepLength( int stepLength );
+    
+    /**
+     * Defines the weight of the value of the pixel visibility when deciding 
+     * which pixel to move to.
+     * @param visibilityWeight
+     */
     void setVisibilityWeight( float visibilityWeight );
+    
+    /**
+     * Defines the weight of the value of the concentration of pheromone on the 
+     * pixel when deciding which pixel to move to
+     * @param pheromoneWeight
+     */
     void setPheromoneWeight( float pheromoneWeight );
+    
+    /**
+     * Moves the ant to the next pixel. If the ant is in an invalid pixel or has
+     * nowhere to go, it gets flagged as dead
+     */
     void move();
+    
+    /**
+     * Answers if the ant is alive
+     * @return true if alive, false otherwise
+     */
     bool isAlive();
+    
+    /**
+     * Deposits pheromone on the trail made by the ant
+     */
     void depositPheromone();
+    
+    /**
+     * Picks an index in an array randomly. The probability of each index 
+     * getting picked is equal to the value in the vector on position equal to the index
+     * @param probabilities Probabilities vector
+     * @return An index in the probabilities vector
+     */
     static int pickIndex( std::vector<float> probabilities );
     static int pickIndex( float* probabilities, int size );
 
